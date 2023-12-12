81b9489473e8ec71f4f881c79c3048fd454bc62b
KirtanSangani <152529236+KirtanSangani@users.noreply.github.com>
2023-12-12 14:15:20 -0500

    Update Heatmap.py

--- Heatmap.py
+++ Heatmap.py
@@ -1,25 +1,14 @@
-import plotly.figure_factory as ff
+import geopandas as gpd
+import matplotlib.pyplot as plt
 
-import numpy as np
-import pandas as pd
+geojson_path = 'North_Carolina.geojson'
+gdf = gpd.read_file(geojson_path)
 
-df_sample = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/minoritymajority.csv')
-df_sample_r = df_sample[df_sample['STNAME'] == 'North Carolina']
+gdf.plot()
+plt.title('Original GeoJSON Data')
+plt.show()
 
-values = df_sample_r['TOT_POP'].tolist()
-fips = df_sample_r['FIPS'].tolist()
-
-endpts = list(np.mgrid[min(values):max(values):4j])
-colorscale = ["#030512","#1d1d3b","#323268","#3d4b94","#3e6ab0",
-              "#4989bc","#60a7c7","#85c5d3","#b7e0e4","#eafcfd"]
-fig = ff.create_choropleth(
-    fips=fips, values=values, scope=['North Carolina'], show_state_data=True,
-    colorscale=colorscale, binning_endpoints=endpts, round_legend_values=True,
-    plot_bgcolor='rgb(229,229,229)',
-    paper_bgcolor='rgb(229,229,229)',
-    legend_title='Population by County',
-    county_outline={'color': 'rgb(255,255,255)', 'width': 0.5},
-    exponent_format=True,
-)
-fig.layout.template = None
-fig.show()
+fig, ax = plt.subplots(1,1, figsize=(10,10))
+gdf.plot(ax=ax, cmap='Y10rRd', alpha = 0.5)
+plt.title('Heatmap from GeoJSON Data')
+plt.show()
