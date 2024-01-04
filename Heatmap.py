52eb65caffe603658b281953944a367b68707d5d
KirtanSangani <152529236+KirtanSangani@users.noreply.github.com>
2024-01-03 23:28:33 -0500

    Update Heatmap.py

--- Heatmap.py
+++ Heatmap.py
@@ -1,14 +1,28 @@
 import geopandas as gpd
+import pandas as pd
 import matplotlib.pyplot as plt
 
-geojson_path = 'North_Carolina.geojson'
-gdf = gpd.read_file(geojson_path)
+# Load the GeoJSON file of North Carolina counties
+nc_counties_path = 'North_Carolina.geojson'
+nc_counties_gdf = gpd.read_file(nc_counties_path)
 
-gdf.plot()
-plt.title('Original GeoJSON Data')
-plt.show()
+# Load your CSV file with data
+data_path = 'North Carolina Drug Deaths - Sheet1.csv'
+data_df = pd.read_csv(data_path)
 
-fig, ax = plt.subplots(1,1, figsize=(10,10))
-gdf.plot(ax=ax, cmap='Y10rRd', alpha = 0.5)
-plt.title('Heatmap from GeoJSON Data')
+# Merge the GeoDataFrame with the data
+merged_gdf = nc_counties_gdf.merge(data_df, how='left', left_on='County', right_on='County')
+
+# Plot the heatmap
+fig, ax = plt.subplots(1, 1, figsize=(12, 8))
+merged_gdf.plot(column='Average Drug Death Rate 2015-2022', cmap='YlOrRd', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True)
+
+# Add county labels
+for x, y, label in zip(merged_gdf.geometry.centroid.x, merged_gdf.geometry.centroid.y, merged_gdf['County']):
+    ax.text(x, y, label, fontsize=8, ha='center', va='center')
+
+# Add a title
+plt.title('North Carolina Heatmap')
+
+# Show the plot
 plt.show()
